23eb4d5c676c1585d9b89b8d14e6e3bd3c2d883b
James Weis <jweis131@gmail.com>
2014-11-19 13:45:44 -0500

    added inName fuc to CAP last name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -142,6 +142,20 @@ function displayWork(){
 
 displayWork();
 
+function inName(name){
+    var splitName = name.trim().split(" ");
+    var lastName = splitName[1].toUpperCase();
+    var firstName = splitName[0].slice(0,1).toUpperCase() + splitName[0].slice(1).toLowerCase();
+    var newName = firstName + " " + lastName;
+    var formattedNewName = HTMLheaderName.replace("%data%", newName);
+    var internationalName = $("#header").prepend(formattedNewName);
+    return internationalName;
+};
+$("#main").prepend(internationalizeButton);
+
+
+
+
 
 
 
